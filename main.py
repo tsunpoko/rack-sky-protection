54242c8b73ef0025b3cd38b5dae1b73c73ba1cee
tsunpoko <exploit.py@gmail.com>
2019-11-05 04:52:50 +0900

    [update] be able to post tweet with GIF.

--- main.py
+++ main.py
@@ -1,13 +1,86 @@
 import os
 import time
+import tweepy
+import random
+####
+import config
 
-def postTweet():
-    
+CONSUMER_KEY = CONSUMER_SECRET = ACCESS_TOKEN = ACCESS_TOKEN_SECRET = ''
+#DIR_NAME = "/var/lib/motion/"
+DIR_NAME = "./tmp/"
+
+def postTweet(file_name):
+    print("[+] postTweet() is called")
+    print(file_name)
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    api = tweepy.API(auth)
+
+    image_path = "./tmp/output.gif"
+
+    text_list = [
+    "沖縄高専 光輝け",
+    "しんどすぎて振動するオタク",
+    "陰スタグラム",
+    "えん（；＿；）",
+    "にんにん",
+    "草オブ草",
+    "自我を保てないんよ\n#自我の崩壊",
+    "skrr skrr",
+    "ストロングゼロで流しそうめんせん？",
+    "はやく退学したい",
+    "夏にはチンポポの花が咲くんよ",
+    #"突然2億円の借金を背負って死ぬ",
+    "今日も一日",
+    "はやくこれになりたい",
+    "男女の友情は成立しません",
+    "順吉が実は童貞ではないという事実、家系ラーメンより好き",
+    "職業 : へのじい",
+    "「chill」って言葉と共にストーリー投稿しておけばインスタグラマーっぽい",
+    "オタク息しとる？",
+    "お願い🙏シンデレラ👠夢は夢で終われない👊動き始めてる🏃輝く日のために✨ウリャ！オイ！ウリャ！オイ！ウリャ！オイ！ウリャ！オイ！ ア～～～ッシャアイグゾー！！🙋ピノキオ！👺プーさん！🐻ミッキー！🐭スティッチ！👽バンビ！👶ドナルド！🐧シンデレラー！👗",
+    "やば❗笑 田所のツイートめっちゃ伸びてるやん！学校では陰キャなのにTwitterでは人気なんやな笑 また学校来いよ❔✊ みんなで遊ぼうぜ😁👊",
+    "ぽなかしゅいた...",
+    "ダメです",
+    "このアカウントが橋本環奈の裏アカということがバレるのも時間の問題だな...",
+    "#安倍政権を許すな",
+    ".@YouWatanabeEins"
+    ]
+
+    api.update_with_media(filename = image_path, status = text_list[random.randint(0, len(text_list) - 1)])
+    #api.update_status(status = text)
+
 
 def detect():
-    pass
+    print("[+] detect() is called.")
 
-def __name__ == "__main__":
+    ls =  os.listdir(DIR_NAME)
+
+    if len(ls) != 1:
+        return
+
+    postTweet(DIR_NAME + ls[0])
+
+if __name__ == "__main__":
+
+    # initialize
+    CONSUMER_KEY = config.CONSUMER_KEY
+    CONSUMER_SECRET = config.CONSUMER_SECRET
+    ACCESS_TOKEN = config.ACCESS_TOKEN
+    ACCESS_TOKEN_SECRET = config.ACCESS_TOKEN_SECRET
+
+    ls =  os.listdir(DIR_NAME)
+
+    if len(ls) > 0:
+        for i in ls:
+            os.remove(DIR_NAME + i)
+
+    print("[+] Initialized!")
+
+
     while True:
         detect()
         time.sleep(3)
+        #time.sleep(30)
