7ea52fe7cecf2fade737c0a4d82638833ed17cf1
tsunpoko <exploit.py@gmail.com>
2019-11-05 20:27:41 +0900

    [fix] "motion" option is fixed

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-sed -i s/"max_movie_time 0"/"max_movie_time 30"/ /etc/motion/motion.conf
+sed -i s/"max_movie_time 0"/"max_movie_time 90"/ /etc/motion/motion.conf
 
 nohup motion > output.log &
 
 wget $CONFIG_URL
 
 python3 main.py
+sed -i s/"-r 10"/"-r 100"/ main.py
