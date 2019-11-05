9700e99e47aaa8d304cd246ab7a3a7e7c677d5e7
tsunpoko <exploit.py@gmail.com>
2019-11-05 21:11:35 +0900

    [fix] change paramaters

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-sed -i s/"max_movie_time 0"/"max_movie_time 90"/ /etc/motion/motion.conf
-
-nohup motion > output.log &
+#sed -i s/"max_movie_time 0"/"max_movie_time 90"/ /etc/motion/motion.conf
 
 wget $CONFIG_URL
 
-python3 main.py
-sed -i s/"-r 10"/"-r 100"/ main.py
+nohup python3 main.py > output.log &
+
+motion
