1c857f10b47a2b3cbea604f0066abaa30dc582ba
tsunpoko <exploit.py@gmail.com>
2019-11-07 18:26:48 +0900

    [update] Correspondence exception

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-#sed -i s/"max_movie_time 0"/"max_movie_time 90"/ /etc/motion/motion.conf
+sed -i s/"max_movie_time 0"/"max_movie_time 90"/ /etc/motion/motion.conf
 
 wget $CONFIG_URL
 
-nohup python3 main.py > output.log &
+#nohup python3 main.py > output.log &
+nohup $(while true; do python main.py ; done) > output.log &
 
 motion
